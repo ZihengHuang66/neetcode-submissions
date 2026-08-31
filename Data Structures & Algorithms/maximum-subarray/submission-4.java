8c7458924a3e8136d7dd45ee70289ca74d2746f7
ZihengHuang66 <danhzh2024@gmail.com>
2026-08-31 17:32:42 +0800

    Add: maximum-subarray - submission-4

---
+++ Data Structures & Algorithms/maximum-subarray/submission-4.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int res = Integer.MIN_VALUE;    //此处是因为可能出现很多负数干扰
+        int currSum = 0;
+        for(int i = 0; i < nums.length; i++) {
+            currSum += nums[i];
+            if(currSum > res) {
+                res = currSum;
+            }
+            if(currSum < 0) {
+                currSum = 0;
+            }
+        }
+        return res;
+    }
+}
