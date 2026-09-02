d7e365d26e0b0e8a2ebd46dbb405b803e4bdcc1c
ZihengHuang66 <danhzh2024@gmail.com>
2026-09-02 12:24:12 +0800

    Add: longest-palindromic-substring - submission-6

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-6.java
@@ -0,0 +1,42 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        String res = s.substring(0, 1);
+        int longest = 1;
+
+        for(int i = 0; i < s.length() - 1; i++) {
+            if(i > 0 && s.charAt(i - 1) == s.charAt(i + 1)) {
+                String curr = s.substring(i - 1, i + 2);
+                int left = i - 2; 
+                int right = i + 2;
+                curr = expand(s, left, right);
+                if(curr.length() > longest) {
+                    res = curr;
+                    longest = curr.length();
+                }
+            } 
+            if(s.charAt(i) == s.charAt(i + 1)) {
+                String curr = s.substring(i, i + 2);
+                int left = i - 1;
+                int right = i + 2;
+                curr = expand(s, left, right);
+                if(curr.length() > longest) {
+                    res = curr;
+                    longest = curr.length();
+                }
+            }
+        }
+        return res;
+    }
+
+    private String expand(String s, int left, int right) {
+        while(left >= 0 && right < s.length() 
+                                && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return s.substring(left + 1, right);
+    }
+}
+
+// "acbbc"
+// "abbbc"
