1bab7b43914c0e040134036774e9a6e77441ca8a
ZihengHuang66 <danhzh2024@gmail.com>
2026-09-03 17:43:51 +0800

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        Set<Integer> visiting = new HashSet<>();
+
+        // build the map
+        for(int i = 0; i < numCourses; i++) {
+            map.put(i, new ArrayList<>());
+        }
+        for(int[] prereq : prerequisites) {
+            map.get(prereq[0]).add(prereq[1]);   
+        }
+
+        // operate dfs, every course must be completed
+        for(int i = 0; i < numCourses; i++) {
+            if(!dfs(i, visiting, map)) return false;
+        }
+        return true;
+    }
+        private boolean dfs(int course, Set<Integer> visiting, Map<Integer, List<Integer>> map) {
+            if(visiting.contains(course)) return false; // find a loop
+            if(map.get(course).isEmpty()) return true;  // no prereqs anymore
+            
+            visiting.add(course);
+            // dfs until we find a loop - F / find no prereqs - T
+            for(int prereq : map.get(course)) {
+                if(!dfs(prereq, visiting, map)) return false;
+            }
+
+            // remove the current course for next use
+            visiting.remove(course);
+            // mark the course as completed
+            map.put(course, new ArrayList<>());
+            return true;
+        }
+}
